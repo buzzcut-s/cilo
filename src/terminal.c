37f2bf3401b77851f7d8a2691d60842e386df872
Piyush Kumar <piyushkumg@gmail.com>
2021-12-06 12:56:15 +0530

    terminal: Disable raw mode at exit
    Restore the users original terminal state when exiting.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/terminal.c
+++ src/terminal.c
@@ -1,13 +1,27 @@
 #include "cilo/terminal.h"
 
+#include <stdlib.h>
+
 #include <termios.h>
 #include <unistd.h>
 
+static struct termios original_terminal_state;
+
+static void disable_raw_mode()
+{
+    /* restore original terminal attributes */
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_terminal_state);
+}
+
 void enable_raw_mode()
 {
-    struct termios raw;
+    /* store original terminal attributes */
+    tcgetattr(STDIN_FILENO, &original_terminal_state);
 
-    tcgetattr(STDIN_FILENO, &raw);
+    if (atexit(disable_raw_mode) != 0)
+        exit(EXIT_FAILURE);
+
+    struct termios raw = original_terminal_state;
 
     /* local modes - echoing off */
     raw.c_lflag &= ~(ECHO);
