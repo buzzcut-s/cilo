cf944ec2692d6e2d83d5620752d0d719ab8d9df6
Piyush Kumar <piyushkumg@gmail.com>
2021-12-06 12:58:01 +0530

    main: Enable raw mode on startup
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <unistd.h>
 
+#include <cilo/terminal.h>
+
 int main(int argc, const char* argv[])
 {
+    enable_raw_mode();
+
     char c = 0;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     {
