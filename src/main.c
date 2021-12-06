12e54bdd041e439a1322fe5b9b9e6dbf8eb9b564
Piyush Kumar <piyushkumg@gmail.com>
2021-12-06 13:20:43 +0530

    main: Display all keypresses
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,3 +1,6 @@
+#include <ctype.h>
+#include <stdio.h>
+
 #include <unistd.h>
 
 #include <cilo/terminal.h>
@@ -9,6 +12,10 @@ int main(int argc, const char* argv[])
     char c = 0;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     {
+        if (iscntrl(c))
+            printf("%d\n", c);
+        else
+            printf("%d ('%c')\n", c, c);
     }
 
     return 0;
