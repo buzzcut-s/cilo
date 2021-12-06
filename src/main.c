0c486024940ab9d233ac47cd97beefa8c8abd6f6
Piyush Kumar <piyushkumg@gmail.com>
2021-12-06 13:51:33 +0530

    main: Add carriage returns when printing newline
    After disabling output post processing, we see that the newline
    characters we’re printing are only moving the cursor down, and not to
    the left side of the screen.
    
    Add carriage returns to our printf() statements. From now on, we’ll
    have to write out the full "\r\n" whenever we want to start a new line.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -13,9 +13,9 @@ int main(int argc, const char* argv[])
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
     {
         if (iscntrl(c))
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         else
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
     }
 
     return 0;
