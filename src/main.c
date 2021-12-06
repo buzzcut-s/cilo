5dcb268b247d0a5e6ec6cd72dcb8e69c13f79da8
Piyush Kumar <piyushkumg@gmail.com>
2021-12-06 12:48:07 +0530

    main: Read characterwise input
    Quit when 'q' is read.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, const char* argv[])
 {
-    printf("Witness.\n");
+    char c = 0;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+    {
+    }
 
     return 0;
 }
