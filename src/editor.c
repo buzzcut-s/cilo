3e096722b6f697695d798cd4ed1e98785a2f8f8f
Piyush Kumar <piyushkumg@gmail.com>
2021-12-07 10:49:45 +0530

    editor: Refactor getting window size fallback path
    What one night of sleep does.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/editor.c
+++ src/editor.c
@@ -91,27 +91,31 @@ static int move_cursor_to_bottom_right()
                                                            : -1;
 }
 
-static int write_cursor_position(uint16_t* out_rows, uint16_t* out_cols)
+static int write_active_position()
 {
     static const char* const DSR_ACTIVE_POSITION = "\x1b[6n";
-    if (write(STDOUT_FILENO, DSR_ACTIVE_POSITION, 4) != 4)
-        return -1;
+    return write(STDOUT_FILENO, DSR_ACTIVE_POSITION, 4) == 4 ? 4
+                                                             : -1;
+}
 
-    char buf[32];
-
+static void read_active_position(char (*buf)[32])
+{
     unsigned int i = 0;
-    while (i < sizeof(buf) - 1)
+    while (i < sizeof(*buf) - 1)
     {
-        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+        if (read(STDIN_FILENO, &(*buf)[i], 1) != 1)
             break;
 
-        if (buf[i] == 'R')
+        if ((*buf)[i] == 'R')
             break;
 
         i++;
     }
-    buf[i] = '\0';
+    (*buf)[i] = '\0';
+}
 
+static int parse_active_position(const char buf[], uint16_t* out_rows, uint16_t* out_cols)
+{
     if (buf[0] != '\x1b' || buf[1] != '[')
         return -1;
 
@@ -121,18 +125,28 @@ static int write_cursor_position(uint16_t* out_rows, uint16_t* out_cols)
     return 0;
 }
 
+static int write_screen_size(uint16_t* out_rows, uint16_t* out_cols)
+{
+    if (write_active_position() == -1)
+        return -1;
+
+    char buf[32];
+    read_active_position(&buf);
+
+    return parse_active_position(buf, out_rows, out_cols);
+}
+
 static int get_window_size(uint16_t* out_rows, uint16_t* out_cols)
 {
     struct winsize current;
 
     if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &current) == -1
-        || current.ws_col == 0
-        || current.ws_row == 0)
+        || current.ws_col == 0 || current.ws_row == 0)
     {
         if (move_cursor_to_bottom_right() == -1)
             return -1;
 
-        return write_cursor_position(out_rows, out_cols);
+        return write_screen_size(out_rows, out_cols);
     }
 
     *out_rows = current.ws_row;
