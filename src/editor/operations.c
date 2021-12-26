3ef5ec82fda6c34382ef7bcc77df9822e4a76bc1
Piyush Kumar <piyushkumg@gmail.com>
2021-12-26 22:56:19 +0530

    editor/operations: Handle backspacing at the beginning of a line
    Straightforward now that all the other helper functions have been
    implemented.
    
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- src/editor/operations.c
+++ src/editor/operations.c
@@ -20,12 +20,22 @@ void editor_op_delete_char()
 {
     if (editor.cursor_y == editor.num_rows)
         return;
+    if (editor.cursor_x == 0 && editor.cursor_y == 0)
+        return;
 
+    struct EditorRow* row = &editor.rows[editor.cursor_y];
     if (editor.cursor_x > 0)
     {
-        er_delete_character(&editor.rows[editor.cursor_y], editor.cursor_x - 1);
-        editor.is_dirty = true;
-
+        er_delete_character(row, editor.cursor_x - 1);
         editor.cursor_x--;
     }
+    else
+    {
+        editor.cursor_x = editor.rows[editor.cursor_y - 1].line_length;
+        er_append_string(&editor.rows[editor.cursor_y - 1], row->line_chars, row->line_length);
+        editor_state_delete_line(editor.cursor_y);
+        editor.cursor_y--;
+    }
+
+    editor.is_dirty = true;
 }
