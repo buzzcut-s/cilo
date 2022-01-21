81b1b79f1d7609b1f404f4043a5044e535fd15ef
Piyush Kumar <piyushkumg@gmail.com>
2022-01-21 21:02:39 +0530

    editor/highlight: Reorder HighlightType
    Signed-off-by: Piyush Kumar <piyushkumg@gmail.com>

--- include/cilo/editor/highlight.h
+++ include/cilo/editor/highlight.h
@@ -4,8 +4,8 @@
 enum HighlightType
 {
     HL_Default = -1,  // NOLINT
-    HL_Number  = 0,   // NOLINT
-    HL_Normal,        // NOLINT
+    HL_Normal  = 0,   // NOLINT
+    HL_Number,        // NOLINT
 };
 
 int eh_highlight_to_color(int highlight);
